6f43b239574157d7caea0e36d7a09d00179776e1
imran <sherlockholmesad221@gmail.com>
2024-11-02 00:06:54 +0600

    edit BookDetails

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 function Home() {
   const [books, setBooks] = useState([]);
-  // console.log(books);
+//   console.log(books);
 
   useEffect(() => {
     async function fetchRandomBooks() {
